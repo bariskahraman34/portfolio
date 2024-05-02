76f1ef98d06b462768bd42a905afe15877c126fd
Barış Kahraman <baris-kahra@hotmail.com>
2024-05-03 01:21:56 +0300

    Proje cardları tasarlandı ve içerisine json'a eklenen verilerin basılması işlemi js tarafında yapıldı. Öğrendiklerim kısmında bulunan elemanlar json dosyasına yerleştirildi ve fetch atılarak alınan veri ekrana basıldı. örnek projeler eklendi.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,51 @@
-const BASE_URL = "assets/data/github-projects.json";
-
-async function fetchURL(){
-    const response = await fetch(BASE_URL);
+const projectsList = document.querySelector('#projects .card-container');
+const toolsList = document.querySelector('#tools .card-container');
+async function fetchURL(url){
+    const response = await fetch(url);
     const data = response.json();
     return data;
 }
 
-async function listData(){
-    const data = await fetchURL();
+async function listProjects(){
+    const data = await fetchURL("assets/data/github-projects.json");
+    projectsList.innerHTML = "";
     data.forEach(project => {
-        console.log(project);
+        projectsList.innerHTML += 
+        `
+        <a href="${project.demo_URL}" target="_blank" class="card">
+            <img src="${project.thumbnail}" alt="">
+            <div class="card-heading">
+                <h3 class="project-name">${project.projectName}</h3>
+                <span class="color-green">${project.year}</span>
+            </div>
+            <div class="languages">
+                <ul class="languages-list">
+                    ${project.technologies.map(tech => `<li>${tech}</li>`).join('')}
+                </ul>
+            </div>
+            <p class="description">
+                ${project.description}
+            </p>
+        </a>
+        `
     });
 }
 
-listData();
+async function listWhatILearned(){
+    const data = await fetchURL("assets/data/whatILearn.json");
+    toolsList.innerHTML = ""
+    data.forEach(tool => {
+        toolsList.innerHTML += 
+        `
+        <div class="card tooltip" data-tooltip="${tool.tool}">
+            ${tool.logo}
+        </div>
+        `
+    })
+}
+
+listWhatILearned();
+
+
+listProjects();
+
