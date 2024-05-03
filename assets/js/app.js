00e97d24f74df7023a6242888f382bd7d4cc89bf
Barış Kahraman <baris-kahra@hotmail.com>
2024-05-03 20:47:27 +0300

    Background u oluşturmak için body nin son halinin height'ını aldım ve her bir row yaklaşık olarak 60px yüksekliğe sahip olduğundan yüksekliği 60 a bölerek genel bir background container'ı oluşturmayı hedefledim. her satırı böylece bulunulan yüksekliğe göre doldurmuş oldum. Oluşturduğum bu fonksiyonu gerekli yerde çağırarak background oluşmasını sağladım.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,23 +12,27 @@ async function listProjects(){
     data.forEach(project => {
         projectsList.innerHTML += 
         `
-        <a href="${project.demo_URL}" target="_blank" class="card">
-            <img src="${project.thumbnail}" alt="">
-            <div class="card-heading">
-                <h3 class="project-name">${project.projectName}</h3>
-                <span class="color-green">${project.year}</span>
-            </div>
-            <div class="languages">
-                <ul class="languages-list">
-                    ${project.technologies.map(tech => `<li>${tech}</li>`).join('')}
-                </ul>
-            </div>
-            <p class="description">
-                ${project.description}
-            </p>
-        </a>
+        <div class="card">        
+            <a href="${project.demo_URL}" target="_blank" >
+                <img src="${project.thumbnail}" alt="">
+                <div class="card-heading">
+                    <h3 class="project-name">${project.projectName}</h3>
+                    <span class="color-green">${project.year}</span>
+                </div>
+                <div class="languages">
+                    <ul class="languages-list">
+                        ${project.technologies.map(tech => `<li>${tech}</li>`).join('')}
+                    </ul>
+                </div>
+                <p class="description">
+                    ${project.description}
+                </p>
+            </a>
+            <a class="github-link" href="${project.source_code_url}">Github Repository <i class="fa-solid fa-chevron-right"></i></a>
+        </div>
         `
     });
+    createBackground()
 }
 
 async function listWhatILearned(){
@@ -44,8 +48,85 @@ async function listWhatILearned(){
     })
 }
 
+
+function createBackground(){
+    const body = document.body;
+    const html = document.documentElement;
+    const height = Math.max(body.scrollHeight, body.offsetHeight,
+        html.clientHeight, html.scrollHeight, html.offsetHeight);
+    const backgorund = document.querySelector('section.background');
+    backgorund.innerHTML = ""
+    for(let i = 0 ; i < height / 60 ; i++){
+        backgorund.innerHTML += 
+        `
+            <div class="row">
+              <div>
+                <i class="fa-solid fa-cookie backgroundIcon"></i>
+                <i class="fa-solid fa-bug-slash backgroundIcon"></i>
+                <i class="fa-solid fa-clapperboard backgroundIcon"></i>
+                <i class="fa-solid fa-earth-oceania backgroundIcon"></i>
+                <i class="fa-solid fa-panorama backgroundIcon"></i>
+                <i class="fa-solid fa-book-atlas backgroundIcon"></i>
+                <i class="fa-solid fa-virus-covid-slash backgroundIcon"></i>
+                <i class="fa-brands fa-pix backgroundIcon"></i>
+                <i class="fa-brands fa-github backgroundIcon"></i>
+                <i class="fa-regular fa-heart backgroundIcon"></i>
+                <i class="fa-brands fa-apple backgroundIcon"></i>
+                <i class="fa-solid fa-clipboard-check backgroundIcon"></i>
+                <i class="fa-solid fa-sailboat backgroundIcon"></i>
+                <i class="fa-brands fa-bots backgroundIcon"></i>
+                <i class="fa-solid fa-bolt backgroundIcon-lightning backgroundIcon"></i>
+                <i class="fa-brands fa-padlet backgroundIcon"></i>
+                <i class="fa-brands fa-android backgroundIcon"></i>
+                <i class="fa-solid fa-battery-full backgroundIcon"></i>
+                <i class="fa-brands fa-gitlab backgroundIcon"></i>
+                <i class="fa-solid fa-bolt backgroundIcon"></i>
+                <i class="fa-solid fa-book-skull backgroundIcon"></i>
+                <i class="fa-solid fa-circle-check backgroundIcon"></i>
+                <i class="fa-solid fa-cubes backgroundIcon"></i>
+                <i class="fa-solid fa-database backgroundIcon"></i>
+                <i class="fa-solid fa-diagram-project backgroundIcon"></i>
+                <i class="fa-brands fa-docker backgroundIcon"></i>
+                <i class="fa-solid fa-ghost backgroundIcon"></i>
+                <i class="fa-brands fa-gitkraken backgroundIcon"></i>
+              </div>
+              <div>
+                <i class="fa-solid fa-cookie backgroundIcon"></i>
+                <i class="fa-solid fa-bug-slash backgroundIcon"></i>
+                <i class="fa-solid fa-clapperboard backgroundIcon"></i>
+                <i class="fa-solid fa-earth-oceania backgroundIcon"></i>
+                <i class="fa-solid fa-panorama backgroundIcon"></i>
+                <i class="fa-solid fa-book-atlas backgroundIcon"></i>
+                <i class="fa-solid fa-virus-covid-slash backgroundIcon"></i>
+                <i class="fa-brands fa-pix backgroundIcon"></i>
+                <i class="fa-brands fa-github backgroundIcon"></i>
+                <i class="fa-regular fa-heart backgroundIcon"></i>
+                <i class="fa-brands fa-apple backgroundIcon"></i>
+                <i class="fa-solid fa-clipboard-check backgroundIcon"></i>
+                <i class="fa-solid fa-sailboat backgroundIcon"></i>
+                <i class="fa-brands fa-bots backgroundIcon"></i>
+                <i class="fa-solid fa-bolt backgroundIcon-lightning backgroundIcon"></i>
+                <i class="fa-brands fa-padlet backgroundIcon"></i>
+                <i class="fa-brands fa-android backgroundIcon"></i>
+                <i class="fa-solid fa-battery-full backgroundIcon"></i>
+                <i class="fa-brands fa-gitlab backgroundIcon"></i>
+                <i class="fa-solid fa-bolt backgroundIcon"></i>
+                <i class="fa-solid fa-book-skull backgroundIcon"></i>
+                <i class="fa-solid fa-circle-check backgroundIcon"></i>
+                <i class="fa-solid fa-cubes backgroundIcon"></i>
+                <i class="fa-solid fa-database backgroundIcon"></i>
+                <i class="fa-solid fa-diagram-project backgroundIcon"></i>
+                <i class="fa-brands fa-docker backgroundIcon"></i>
+                <i class="fa-solid fa-ghost backgroundIcon"></i>
+                <i class="fa-brands fa-gitkraken backgroundIcon"></i>
+              </div>
+            </div>
+        `
+    }
+    
+}
+
+
+
 listWhatILearned();
-
-
 listProjects();
-
